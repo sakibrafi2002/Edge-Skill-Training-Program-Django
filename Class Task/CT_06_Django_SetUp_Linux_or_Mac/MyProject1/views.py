c1fd1cdf2d40065843a6c2005b8dbbf83afe02b7
MUSAFIR <sakibrafi2001@gmail.com>
2024-10-11 19:56:14 +0600

    Add files via upload

---
+++ Class Task/CT_06_Django_SetUp_Linux_or_Mac/MyProject1/views.py
@@ -0,0 +1,8 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+# def hello_django(request):
+#     return HttpResponse("Hello Django")
+
+def hello_django(request):
+    return render(request,'hello.html')
